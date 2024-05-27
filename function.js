0b3b32df505f13723b9e74ae84a7cb8a23192761
Lucas Silva <lucasvida2005@gmail.com>
2024-05-27 20:08:44 -0300

    adding function file

---
+++ function.js
@@ -0,0 +1,10 @@
+// TODO: MOSTRAR FUNCIONALIDADE DO CALLBACK (function)
+// TODO: map, forEach, setTimeout, reduce
+
+const callback = function (param) {
+    console.log(param)
+}
+
+const outra = callback
+
+outra("teste")
