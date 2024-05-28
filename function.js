88bf170c506bbbef609373736aedc5876056f366
Lucas Silva <lucasvida2005@gmail.com>
2024-05-27 22:38:47 -0300

    new comments and callback methods

--- function.js
+++ function.js
@@ -1,10 +1,32 @@
-// TODO: MOSTRAR FUNCIONALIDADE DO CALLBACK (function)
-// TODO: map, forEach, setTimeout, reduce
+// TODO: Função de Callback, utilização com map, forEach, setTimeout e reduce
+// * Callback é uma função passada como argumento para outra função
 
-const callback = function (param) {
-    console.log(param)
+// ------------ Callback ----------- //
+function showResult(result) {
+    console.log(`The result is: ${result}`)
 }
 
-const outra = callback
+function calc(calculation, funcResult) {
+    let result = eval(calculation)
+    funcResult(result)
+}
 
-outra("teste")
+calc('10+20', showResult)
+
+// -------------- Map -------------- //
+const cars = ['Toyota', 'Honda', 'Chevrolet', 'Ferrari', 'Porsche']
+
+const allCars = cars.map((car) => {
+    console.log(car);
+})
+
+// ------------ forEach ------------ //
+const planes = ['ACS-100 Sora', 'Aeromot AMT-600 Guri', 'Aeronave Alvear', 'Aerotec T-23 Uirapuru', 'Anequim']
+
+const allPlanes = planes.forEach((plane) => {
+    console.log(plane)
+})
+
+// ----------- setTimeout ---------- //
+
+// ------------- reduce ------------ //
